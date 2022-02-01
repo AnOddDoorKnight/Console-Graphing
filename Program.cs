12eeeb460e3b7f6c498fc6e0e24b6cb03313c69d
AnOddDoorKnight <91893431+AnOddDoorKnight@users.noreply.github.com>
2022-02-01 15:10:33 -0800

    Actually made the graph work, but has an issue about how if the lowest number is not 0, it will have a problem

--- Program.cs
+++ Program.cs
@@ -3,11 +3,15 @@ using System.Linq;
 using System;
 static class Master
 {
-	static BarGraph graph = new(new int[] { 9, 5, 2, 6 });
+	static BarGraph graph = new(new int[] { });
 	static void Main()
 	{
-		Console.WriteLine($"9526");
-		foreach (Unnamed i in graph.Data.ToArray()) Console.Write(i.Value);
+		Random Random = new Random();
+		graph.HeightLength = 9;
+		int ii = Random.Next(2, 15);
+		for(int i = 0; i != ii; i++) graph.Add(Random.Next(2, 32));
+		
+		Console.WriteLine($"{graph}{graph.BaseString()}");
 	}
 }
 //public sealed class PointGraph : Graph
@@ -24,18 +28,36 @@ public sealed class BarGraph : Graph
 		// Add more types here
 		else throw new InvalidCastException();
 	}
-	public override string ToString() => throw new NotImplementedException();
+	public override string ToString() 
+	{
+		string output = "", line = "line-{0}";
+		for (uint i = HeightLength; i > 0; i--) 
+		{
+			output += $"{line.Replace("{0}", i.ToString())}) ";
+			for (int ii = 0; ii < Data.Count; ii++)
+			{
+				output += $"-{(Data[ii].Length >= i ? "#" : "-")}-";
+			}
+			output += "\n";
+		}
+		return output;
+	}
+	public string BaseString() => base.ToString();
 }
 public abstract class Graph
 {
-	public Graph(List<int> data)
+	public List<Unnamed> Data = new();
+	private uint _heightL;
+	public uint HeightLength { get => _heightL; set { _heightL = value; Update(); } }
+	// Constructors
+	public Graph(uint height = 10) { _heightL = height; }
+	public Graph(List<int> data, uint height = 10)
 	{
+		_heightL = height;
 		foreach (int i in data) Data.Add(new Unnamed(i, null));
 		Update();
 	}
-	public Graph(int[] data) : this(data.ToList()) { }
-	// Data Argument Transfer
-	public List<Unnamed> Data = new();
+	public Graph(int[] data, uint height = 10) : this(data.ToList(), height) { }
 	public void Add(int value)
 	{
 		Data.Add(new Unnamed(value, null));
@@ -45,35 +67,54 @@ public abstract class Graph
 	// Methods
 	public void Update()
 	{
+		if (Data.Count == 0) return;
 		Sort();
 		UpdateLengths();
 	}
 	protected void UpdateLengths()
 	{
-		//Take the array of values in Data, and spread them as a byte ranging from 1-10
+		if (Data.Count == 0) return;
+		double largest = Data[^1].Value, 
+			smallest = Data[0].Value, 
+			height = largest - smallest;
+		Data[^1] = new Unnamed((int)largest, HeightLength);
+		Data[0] = new Unnamed((int)smallest, 0);
+		double percentagePerPoint = 1d / height;
+		// It starts at smallest instead of zero, hence height
+		for (int i = 1; i < Data.Count - 1; i++)
+		{
+			Data[i] = new Unnamed(Data[i].Value, (byte)Math.Truncate((Data[i].Value / (double)height) * HeightLength));
+			if (Data[i].Length > HeightLength) throw new IndexOutOfRangeException($"Number is too high: {Data[i].Length} > {HeightLength}, index of {i} out of {Data.Count}. {smallest} as lowest, {largest} as highest, {height} as cap");
+		}
+		
 	}
 	protected void Sort()
 	{
-		for (int i = 0; i > Data.Count; i++)
-			if (Data[i].Value > Data[i + 1].Value)
-			{
-				int[] tempVars = {Data[i].Value, Data[i++].Value };
-				Data[i] = new Unnamed(tempVars[0], null);
-				Data[i++] = new Unnamed(tempVars[1], null);
-				i = -1;
-			}
+		double[] values = new double[Data.Count];
+		for (int i = 0; i < values.Length; i++) values[i] = Data[i].Value;
+		Array.Sort<double>(values);
+		for (int i = 0; i < values.Length; i++) Data[i] = new Unnamed(values[i], null);
 	}
-	public override abstract string ToString();
+	public override string ToString()
+	{
+		string output = "Using Default String...\n";
+		foreach (Unnamed i in Data) output += $"{i.ToString()}\n";
+		return output;
+	}
 	public abstract object Convert<T>() where T : Graph;
-
+	protected static T Convert<T>(bool True) where T : Graph
+	{
+		throw new NotImplementedException();	
+	}
 }
 public struct Unnamed
 {
-	public int Value;
-	public byte? Length;
-	public Unnamed(int value, byte? length)
+	public double Value;
+	public uint? Length;
+	public Unnamed(double value, uint? length)
 	{
 		Value = value;
 		Length = length;
 	}
+	public override string ToString() => $"Value: {Value}, Length: {(Length == null ? "Null" : Length)}";
 }
