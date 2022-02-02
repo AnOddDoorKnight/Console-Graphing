cd9a50cf34875de7f0aa75fc6f883a8d7b6bcd5f
AnOddDoorKnight <gooeycuppy@live.com>
2022-02-02 07:44:36 -0800

    Fixed the bug mentioned before, and fixed an issue where a single digit can shift it a bit

--- Program.cs
+++ Program.cs
@@ -3,15 +3,21 @@ using System.Linq;
 using System;
 static class Master
 {
-	static BarGraph graph = new(new int[] { });
+	static BarGraph graph = new();
+	static Master()
+	{
+		Console.WindowWidth += 50;
+		Console.WindowHeight += 10;
+	}
 	static void Main()
 	{
-		Random Random = new Random();
-		graph.HeightLength = 9;
+		Random Random = new();
+		graph.HeightLength = 15;
 		int ii = Random.Next(2, 15);
-		for(int i = 0; i != ii; i++) graph.Add(Random.Next(2, 32));
-		
+		for (int i = 0; i != ii; i++) graph.Add(Random.Next(2, 32));
+
 		Console.WriteLine($"{graph}{graph.BaseString()}");
+
 	}
 }
 //public sealed class PointGraph : Graph
@@ -20,6 +26,7 @@ static class Master
 //}
 public sealed class BarGraph : Graph
 {
+	public BarGraph(uint height = 10) : base(height) { }
 	public BarGraph(int[] values) : base(values) { }
 	public override object Convert<T>()
 	{
@@ -28,17 +35,20 @@ public sealed class BarGraph : Graph
 		// Add more types here
 		else throw new InvalidCastException();
 	}
-	public override string ToString() 
+	public override string ToString()
 	{
-		string output = "", line = "line-{0}";
-		for (uint i = HeightLength; i > 0; i--) 
+		string output = "", lineReadonly = "line-{0}) ", line;
+		for (uint i = HeightLength; i > 0; i--)
 		{
-			output += $"{line.Replace("{0}", i.ToString())}) ";
+			byte heightLengthLength = (byte)(HeightLength.ToString().Length + lineReadonly.Replace("{0}", "").Length);
+			line = lineReadonly.Replace("{0}", i.ToString());
+			while (line.Length < heightLengthLength) 
+				line += " "; // This line may not work, due to it being a single string instead of a line
 			for (int ii = 0; ii < Data.Count; ii++)
 			{
-				output += $"-{(Data[ii].Length >= i ? "#" : "-")}-";
+				line += $"-{(Data[ii].Length >= i ? "#" : "-")}-";
 			}
-			output += "\n";
+			output += $"{line}\n";
 		}
 		return output;
 	}
@@ -46,7 +56,7 @@ public sealed class BarGraph : Graph
 }
 public abstract class Graph
 {
-	public List<Unnamed> Data = new();
+	public List<GraphLine> Data = new();
 	private uint _heightL;
 	public uint HeightLength { get => _heightL; set { _heightL = value; Update(); } }
 	// Constructors
@@ -54,16 +64,16 @@ public abstract class Graph
 	public Graph(List<int> data, uint height = 10)
 	{
 		_heightL = height;
-		foreach (int i in data) Data.Add(new Unnamed(i, null));
+		foreach (int i in data) Data.Add(new GraphLine(i, null));
 		Update();
 	}
 	public Graph(int[] data, uint height = 10) : this(data.ToList(), height) { }
 	public void Add(int value)
 	{
-		Data.Add(new Unnamed(value, null));
+		Data.Add(new GraphLine(value, null));
 		Update();
 	}
-	public virtual Unnamed this[int index] => Data[index];
+	public virtual GraphLine this[int index] => Data[index];
 	// Methods
 	public void Update()
 	{
@@ -73,45 +83,46 @@ public abstract class Graph
 	}
 	protected void UpdateLengths()
 	{
-		if (Data.Count == 0) return;
-		double largest = Data[^1].Value, 
-			smallest = Data[0].Value, 
+		if (Data.Count < 2) return;
+		double largest = Data[^1].Value,
+			smallest = Data[0].Value,
 			height = largest - smallest;
-		Data[^1] = new Unnamed((int)largest, HeightLength);
-		Data[0] = new Unnamed((int)smallest, 0);
+		Data[0] = new GraphLine(smallest, 0);
+		Data[^1] = new GraphLine(largest, HeightLength);
+		List<double> accountedValues = new();
+		foreach (GraphLine i in Data) accountedValues.Add(i.Value - smallest);
+		double[] lengthsPerValue = new double[Data.Count];
+		lengthsPerValue[0] = 0;
+		lengthsPerValue[^1] = HeightLength - smallest;
 		double percentagePerPoint = 1d / height;
 		// It starts at smallest instead of zero, hence height
 		for (int i = 1; i < Data.Count - 1; i++)
-		{
-			Data[i] = new Unnamed(Data[i].Value, (byte)Math.Truncate((Data[i].Value / (double)height) * HeightLength));
-			if (Data[i].Length > HeightLength) throw new IndexOutOfRangeException($"Number is too high: {Data[i].Length} > {HeightLength}, index of {i} out of {Data.Count}. {smallest} as lowest, {largest} as highest, {height} as cap");
-		}
-		
+			Data[i] = new GraphLine(Data[i].Value, (uint)Math.Truncate((accountedValues[i] / (double)height) * HeightLength));
 	}
 	protected void Sort()
 	{
 		double[] values = new double[Data.Count];
 		for (int i = 0; i < values.Length; i++) values[i] = Data[i].Value;
-		Array.Sort<double>(values);
-		for (int i = 0; i < values.Length; i++) Data[i] = new Unnamed(values[i], null);
+		Array.Sort(values);
+		for (int i = 0; i < values.Length; i++) Data[i] = new GraphLine(values[i], null);
 	}
 	public override string ToString()
 	{
 		string output = "Using Default String...\n";
-		foreach (Unnamed i in Data) output += $"{i.ToString()}\n";
+		foreach (GraphLine i in Data) output += $"{i}\n";
 		return output;
 	}
 	public abstract object Convert<T>() where T : Graph;
 	protected static T Convert<T>(bool True) where T : Graph
 	{
-		throw new NotImplementedException();	
+		throw new NotImplementedException();
 	}
 }
-public struct Unnamed
+public struct GraphLine
 {
 	public double Value;
 	public uint? Length;
-	public Unnamed(double value, uint? length)
+	public GraphLine(double value, uint? length)
 	{
 		Value = value;
 		Length = length;
